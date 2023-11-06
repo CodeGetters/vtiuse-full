0c659405cf9fc2c56220a3eaa225b8b9e529df65
Johnson Xin <2775145275@qq.com>
2023-11-06 20:33:08 +0800

    feat: ✨ integrated swagger

--- packages/back/src/main.ts
+++ packages/back/src/main.ts
@@ -1,15 +1,22 @@
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-declare const module: any;
+import { blue } from "kolorist";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Swagger example")
+    .setDescription("The swagger API description")
+    .setVersion("1.0.0")
+    .addTag("example")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
+
   await app.listen(3000);
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
+  console.log(blue(`[API Docs]${await app.getUrl()}/docs`));
+  console.log(blue(`[Test API]${await app.getUrl()}`));
 }
 bootstrap();
