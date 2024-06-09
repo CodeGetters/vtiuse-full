b0c3af17912ed7da67028334801254ffd617cd16
Simon Johnson <2775145275@qq.com>
2024-06-09 12:40:50 +0800

    chore: ♻️ update 依赖 (#8)

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -2,8 +2,6 @@
 
 # delete all node_modules
 
-current_dir=$(pwd)
-
-find "$current_dir" -name "node_modules" -type d -exec rm -rf {} +
+find . -name "node_modules" -type d -exec rm -rf {} +
 
 echo "已删除所有 node_modules 文件夹"
